3858665e004eb47704ef94d701715a39ab9272be
also SadVanity <44367336+decadentCoder@users.noreply.github.com>
2018-10-25 16:07:46 +0300

    learning GitHub Desktop
    jjj

--- src/javaPackage/JavaProgram.java
+++ src/javaPackage/JavaProgram.java
@@ -2,7 +2,7 @@ package javaPackage;
 
 public class JavaProgram
 {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
